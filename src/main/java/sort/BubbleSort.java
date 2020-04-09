04511fd16752e8c8caae446c132f6471108227fd
Administrator <1548021143@qq.com>
2020-04-09 11:13:26 +0800

    zl
    项目初始化

---
+++ src/main/java/sort/BubbleSort.java
@@ -0,0 +1,15 @@
+package sort;
+
+/**
+ * @author zl
+ * 冒泡排序
+ */
+public class BubbleSort {
+    static public void sort(int[] arr){
+
+    }
+
+    static public <T> void sort(T[] t){
+
+    }
+}
